5c14f2719aff3c09572042799e66c7b9ee7f5a43
Likhith <23btrcl257@jainuniversity.ac.in>
2024-10-13 16:10:22 +0530

    modify sort.py

--- sort.py
+++ sort.py
@@ -1,13 +1,13 @@
-list=[2,7,1,15,10]
+list=[2,7,1,15,10] #list
 list.sort() #sorting
 print(list)
 list.append(9) #append
 print(list)
-list.extend([20,21])
+list.extend([20,21]) #extend
 print(list)
-list.insert(5,30)
+list.insert(5,30) #insert
 print(list)
-list.remove(1)
+list.remove(1) #remove
 print(list)
-list.pop()
+list.pop() #pop
 print(list)
