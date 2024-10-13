ee4faf3f8e2b257e81e8cf8d3642fbb11e58b6a5
Likhith <23btrcl257@jainuniversity.ac.in>
2024-10-13 12:41:26 +0530

    list operations

--- sort.py
+++ sort.py
@@ -1,3 +1,13 @@
 list=[2,7,1,15,10]
-list.sort()
+list.sort() #sorting
+print(list)
+list.append(9) #append
+print(list)
+list.extend([20,21])
+print(list)
+list.insert(5,30)
+print(list)
+list.remove(1)
+print(list)
+list.pop()
 print(list)
